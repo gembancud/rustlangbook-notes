949a7046340aa9ced2fdb569ac63ea2d528029a0
Gil Emmanuel Bancud <gembancud@gmail.com>
2023-01-21 11:48:18 +0800

    Added Chapt17Blog

---
+++ chapt17blog/src/main.rs
@@ -0,0 +1,124 @@
+struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(self)
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
+
+    pub fn reject(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.reject())
+        }
+    }
+
+    pub fn edit(&mut self, content: &str) {
+        if self.state.take().unwrap().editable() {
+            self.content = content.to_string();
+        }
+    }
+}
+
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
+    fn reject(self: Box<Self>) -> Box<dyn State>;
+    fn editable(self: Box<Self>) -> bool {
+        false
+    }
+}
+
+struct Draft {}
+
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview { approve_count: 0 })
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn editable(self: Box<Self>) -> bool {
+        true
+    }
+}
+
+struct PendingReview {
+    approve_count: u32,
+}
+
+impl PendingReview {
+    fn add_approve_count(&mut self) {
+        self.approve_count += 1;
+    }
+}
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        if self.approve_count == 1 {
+            self
+        } else {
+            Box::new(PendingReview {
+                approve_count: self.approve_count + 1,
+            })
+        }
+    }
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Draft {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
